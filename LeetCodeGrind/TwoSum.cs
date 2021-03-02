bc75266a20c58148a2086646a4668796f29d0030
Jake Choi <jake.choi@Norimaconsulting.com>
2021-03-01 21:06:36 -0600

    Add project files.

---
+++ LeetCodeGrind/TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeGrind
+{
+    public class TwoSum
+    {
+        //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        //You can return the answer in any order.
+
+        //Example 1:
+        //Input: nums = [2,7,11,15], target = 9
+        //Output: [0,1]
+        //Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+        public int[] TwoSums(int[] nums, int target)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                //if, in the dictionnary- there exists a complement to the current element of nums array- that and this is the pair
+                //that adds up to the target. 
+                if (numbers.ContainsKey(target - nums[i]))
+                    return new int[] {i, numbers[target - nums[i]] };
+                numbers.Add(nums[i], i);
+            }
+            throw new Exception();
+        }
+
+    }
+}
---
+++ LeetCodeGrind/TwoSumsV2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeGrind
+{
+    class TwoSumsV2
+    {
+        //Given an array of integers numbers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+        //Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+        //You may assume that each input would have exactly one solution and you may not use the same element twice.
+        public int[] TwoSumV2(int[] numbers, int target)
+        {
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (nums.ContainsValue(target - numbers[i]))
+                    return new int[] { nums.FirstOrDefault(x => x.Value == target - numbers[i]).Key + 1, i + 1 };
+                nums.Add(i, numbers[i]);
+            }
+            throw new Exception();
+        }
+    }
+}
